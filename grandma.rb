7202121aca3e394a7631affdb3bd684c7700e230
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-09 06:00:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,15 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+def speak_to_grandma(message)
+  if message.upcase == message
+    return message
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
+  end
+end
+
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
